ba5a41406590c90c1ff0aff41877b5195e88f48d
ranjitlp <59004715+ranjitlp@users.noreply.github.com>
2023-07-13 13:42:50 -0500

    Add files via upload

--- Bakefile.py
+++ Bakefile.py
@@ -1,2 +1,19 @@
-fdf = baker.__dict__['fdf']
-fdf("x","*.gz","gunzip *.gz")
+import glob
+
+
+class bread() :
+   def __init__(self,bake) :
+      self.dummy = 0
+      self.bake = bake
+ 
+   def run(self) :
+      #   for y in glob.glob("a*.gz") :
+      for y in (["abc.gz"]) :
+         #self.bake.fdf("unzip:none","abc:"+y,"gunzip "+y)
+         self.bake.fdf("unzip:none","abc:"+y,"sleep 10")
+
+      self.bake.fdf("move:copy","xyz:def","cp def xyz")
+      self.bake.fdf("copy:unzip","def:abc","cp abc def")
+      
+      print ("hello world")
+      
